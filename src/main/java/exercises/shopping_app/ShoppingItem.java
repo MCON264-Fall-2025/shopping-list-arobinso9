b3300b6702c1cf52dcffb8c65f37e4b52cc10618
arobinso9 <arobinso9@student.touro.edu>
2025-11-25 17:32:14 -0500

    HW completed

--- src/main/java/exercises/shopping_app/ShoppingItem.java
+++ src/main/java/exercises/shopping_app/ShoppingItem.java
@@ -13,4 +13,36 @@ package exercises.shopping_app;
  */
 public class ShoppingItem implements Comparable<ShoppingItem> {
     //TODO
+    String aisle;
+    String name;
+
+    public ShoppingItem(String aisle, String name) {
+        this.aisle = aisle;
+        this.name = name;
+    }
+    public String getAisle() {
+        return aisle;
+    }
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("ShoppingItem" + name);
+        sb.append("Aisle" + aisle);
+        return sb.toString();
+    }
+    @Override
+    public int compareTo(ShoppingItem o) {
+        int aisleCompare = this.aisle.compareTo(o.aisle);
+        if (aisleCompare != 0) {
+            return aisleCompare;
+        }
+        int nameCompare = this.name.compareTo(o.name);
+        if (nameCompare != 0) {
+            return nameCompare;
+        }
+        return 0;
+    }
 }
